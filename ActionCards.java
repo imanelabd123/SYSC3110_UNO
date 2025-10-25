fc556a13060f9c2407693bf23968eb39f32dcc29
naima-mamun <146744959+naima-mamun@users.noreply.github.com>
2025-10-25 02:46:58 -0400

    Update javadoc comments ActionCards.java

--- ActionCards.java
+++ ActionCards.java
@@ -1,9 +1,27 @@
-public class ActionCards extends Card{
+/**
+* Handles special effects of UNO action cards such as, Skip, Reverse, Draw, and Wild cards.
+*
+* @author Amreen Shahid
+* @version 1.0 October 24, 2025
+*/
 
+public class ActionCards extends Card{
+    /**
+    * Constructs ActionCard with specified colour and value.
+    *
+    * @param colour the colour of the card.
+    * @param value the value of the card.
+    */
     public ActionCards(Colours colour, Values value){
         super(colour, value);
     }
-
+    /**
+    * Processes the action card effect based on the card's value.
+    *
+    * @param game The GameFlow instance to execute game actions.
+    * @param nextPlaver player who's next in turn order.
+    * @param currPlaver player who played the action card.
+    */
     public void processActionCard(GameFlow game, Player nextPlayer, Player currPlayer) {
         if(getValue() == Values.DRAW_ONE) {
             game.drawOne(nextPlayer);
