d5f8b80e9bfed10f42bda3aa9ce504542cc5a473
imanelabd123 <imanelabd123@gmail.com>
2025-10-26 00:17:47 -0400

    Update PlayerTest.java

--- PlayerTest.java
+++ PlayerTest.java
@@ -1,1 +1,21 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.List;
 
+
+public class PlayerTest{
+  
+  @Test
+  public void testPlayerCreation(){
+    Player player = new Player("Iman");
+    assertEquals("Iman", player.getName());
+    assertEquals("Iman", player.toString());}
+
+   
+  @Test
+  public void testAddCardToDeckPlayer(){
+    Player player = new Player("Amreen");
+    assertTrue(player.get)
+  
+  
+}
