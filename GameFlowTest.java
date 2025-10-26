a248f989f0c5cef011b013959614365454961f2c
imanelabd123 <imanelabd123@gmail.com>
2025-10-26 19:34:35 -0400

    Update GameFlowTest.java

--- GameFlowTest.java
+++ GameFlowTest.java
@@ -1,7 +1,6 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.List;
-import java.io.ByteArrayInputStream;
 import java.util.Scanner;
 import java.lang.reflect.Field;
 
@@ -9,5 +8,13 @@ public class GameFlowTest{
   private GameFlow game;
   private Player player1, player2;
   @Before
-  public void 
+  public void setUp() throws Exception{
+    game = new GameFlow();
+    player1 = new Player("John");
+    player2 = new Player("Mark");
+
+    Field scoreField = GameFlow.class.
+    
+    
+  }
 }
