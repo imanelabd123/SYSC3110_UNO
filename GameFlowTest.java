32e17360f922c48c4f178f6825e06559587e12f9
imanelabd123 <imanelabd123@gmail.com>
2025-10-27 02:07:53 -0400

    Update GameFlowTest.java

--- GameFlowTest.java
+++ GameFlowTest.java
@@ -122,7 +122,7 @@ public class GameFlowTest{
     int score = game.score(player1);
     assertEquals(71, score); 
   }
- private void setScannerInput(String text)throws Exception{
+ private void setScannerInput(String text) throws Exception{
    Field fields = GameFlow.class.getDeclaredField("input");
    fields.setAccessible(true);
    fields.set(game, new Scanner(new ByteArrayInputStream(text.getBytes())));
