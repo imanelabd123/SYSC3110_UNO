fa6135507c9b72e24731c63d12e4bae796037e92
imanelabd123 <imanelabd123@gmail.com>
2025-10-27 14:15:23 -0400

    Update CardTest.java

--- CardTest.java
+++ CardTest.java
@@ -1,32 +1,42 @@
 /*** @author Iman Elabd
 * @verion 1.2 October 25, 2025
+* test cases for CardTest
 */
 
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class CardTest{
-  
+  /***
+  this is test  for the card creation
+  */
   @Test
   public void testCardCreation(){
     Card card = new Card(Card.Colours.RED, Card.Values.FIVE);
     assertEquals(Card.Colours.RED, card.getColour());
-    assertEquals(Card.Colours.FIVE, card.getValue());}
+    assertEquals(Card.Values.FIVE, card.getValue());}
 
-   
+  /***
+  this is test for the card created for setColour() and setValue
+  */
+
   @Test
   public void testCardSetters(){
     Card card = new Card(Card.Colours.YELLOW, Card.Values.TWO);
     card.setColour(Card.Colours.GREEN);
-    card.setValue(Card.Values.SEVEN)
-    assertEquals(Card.Colours.Green, card.getColour());
+    card.setValue(Card.Values.SEVEN);
+    assertEquals(Card.Colours.GREEN, card.getColour());
     assertEquals(Card.Values.SEVEN, card.getValue());}
-  
+  /***
+  this is test for toString() for both colour and value 
+  */
   @Test
   public void testCardOutputColour(){
     Card card = new Card(Card.Colours.RED, Card.Values.FIVE);
     assertEquals("RED FIVE", card.toString());}
-  
+  /***
+  this is test for toString() for value. 
+  */
   @Test
    public void testCardOutputWild(){
     Card card = new Card(null, Card.Values.WILD);
